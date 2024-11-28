8386881e09208c6771ee6235c3be9eb5429fbc14
Namchai Hangkasee <s6604062620158@email.kmutnb.ac.th>
2024-11-28 18:35:52 +0700

    Version 1 add if else

--- Lab 1.cpp
+++ Lab 1.cpp
@@ -4,12 +4,11 @@ int main (){
 	int num1=100;
 	int num2=500;
 	
-	if (num1>num2){
-		
+		if (num1>num2){
+
 		printf(" num1 is greater than num2");
-	}
-	
-	if (num2>num1){
+	} else {
 		printf("num2 is greater than num1");
 	}
 }
+
